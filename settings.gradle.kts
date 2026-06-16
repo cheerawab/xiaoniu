917cce0d167fca66fe320b19ab0f69f678967349
Cheer_awab <cheerawab@gmail.com>
2026-06-16 15:22:33 +0800

    fix: remove invalid extension block from settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,3 @@ include(":01_purpur-swm-framework")
 include(":02_purpur-swm-partygame")
 include(":03_world-converter")
 include(":04_plugin-suite")
-
-extension(project(":04_plugin-suite")) {
-    build.gradle.kts
-}
